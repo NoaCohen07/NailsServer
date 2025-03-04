145180e55d32fbee131d82e27554b429d59d3a33
User <User@LAB3-3>
2025-03-04 14:03:23 +0200

    keep fixing chats

---
+++ NailsServer/DBScripts/BackupRestore.sql
@@ -0,0 +1,15 @@
+﻿ --REPLACE YOUR DATABASE LOGIN AND PASSWORD IN THE SCRIPT BELOW
+
+Use master
+Go
+
+-- Create a login for the admin user
+CREATE LOGIN [NailsLogin] WITH PASSWORD = '12345';
+Go
+
+--so user can restore the DB!
+ALTER SERVER ROLE sysadmin ADD MEMBER [NailsLogin];
+Go
+
+Create Database NailsDB
+Go
